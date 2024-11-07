3cd8f8f287b768d90c67c879932ecd85a0768cd5
Andres Muñoz Taborda <andreslx11@gmail.com>
2024-11-07 18:49:10 -0500

    test.

---
+++ src/test/java/com/example/managementpharmacy/ManagementPharmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.managementpharmacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementPharmacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
