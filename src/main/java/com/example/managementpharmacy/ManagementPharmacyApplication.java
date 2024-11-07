a2d1f25b16fe6ccab1b1c982125dadfefe14494f
Andres Muñoz Taborda <andreslx11@gmail.com>
2024-11-07 18:26:35 -0500

    feat: Add main application class for Spring Boot application initialization.

---
+++ src/main/java/com/example/managementpharmacy/ManagementPharmacyApplication.java
@@ -0,0 +1,13 @@
+package com.example.managementpharmacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementPharmacyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementPharmacyApplication.class, args);
+	}
+
+}
