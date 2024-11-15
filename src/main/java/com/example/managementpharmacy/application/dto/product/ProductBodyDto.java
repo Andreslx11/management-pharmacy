b3891d2d99f53826ed7b350a09096e257d2bad4c
Andres Muñoz Taborda <andreslx11@gmail.com>
2024-11-15 15:56:24 -0500

    feat: add validation groups for creation.

--- src/main/java/com/example/managementpharmacy/application/dto/product/ProductBodyDto.java
+++ src/main/java/com/example/managementpharmacy/application/dto/product/ProductBodyDto.java
@@ -4,6 +4,7 @@ import com.example.managementpharmacy.persistence.enums.entity.Category;
 import com.example.managementpharmacy.persistence.enums.entity.Concentration;
 import com.example.managementpharmacy.persistence.enums.entity.Presentation;
 
+import com.example.managementpharmacy.shared.util.Create;
 import jakarta.validation.constraints.*;
 import lombok.*;
 
@@ -20,49 +21,49 @@ public class ProductBodyDto {
     // DTO  to create and update
 
 
-    @NotBlank(message = "Trade name is required")
+    @NotBlank(message = "Trade name is required", groups = { Create.class})
     @Size(min = 3, max = 255, message = "Trade name must be between 3 and 255 characters")
     private String tradeName;
 
     private String genericName;
 
-    @NotBlank(message = "Laboratory is required")
+    @NotBlank(message = "Laboratory is required", groups = { Create.class})
     @Size(min = 3, max = 225, message = "Laboratory must be between 3 and 255 characters")
     private String laboratory;
 
-    @NotNull(message = "Presentation is required")
+    @NotNull(message = "Presentation is required", groups = { Create.class})
     private Presentation presentation;
 
-    @NotNull(message = "Concentration is required")
+    @NotNull(message = "Concentration is required", groups = { Create.class})
     private Concentration concentration;
 
-    @NotNull(message = "Stock is required")
+    @NotNull(message = "Stock is required", groups = { Create.class})
     private Integer stock;
 
-    @NotNull(message = "Sale price is required")
+    @NotNull(message = "Sale price is required", groups = { Create.class})
     @DecimalMin(value = "0.0", inclusive = false, message = "The price must be greater than 0")
     private BigDecimal salePrice;
 
-    @NotNull(message = "Expiration date is required")
-    @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "Date format must be dd-MM-yyyy")
+    @NotNull(message = "Expiration date is required", groups = { Create.class})
+    @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "Date format must be yyyy-MM-dd")
     private String expirationDate;
 
-    @NotNull(message = "Category is required")
+    @NotNull(message = "Category is required", groups = { Create.class})
     private Category category;
 
-    @NotBlank(message = "Invima registration is required")
+    @NotBlank(message = "Invima registration is required", groups = { Create.class})
     @Size(min = 5, max = 225, message = "Invima registration must be between 5 and 255 characters")
     private String invimaRegistration;
 
-    @NotNull(message = "Supplier ID is required")
+    @NotNull(message = "Supplier ID is required", groups = { Create.class})
     @Positive(message = "Supplier ID must be a positive number")
     private Long supplierId;
 
-    @NotBlank(message = "Description is required")
+    @NotBlank(message = "Description is required", groups = { Create.class})
     @Size(max = 255, message = "Description must be less than 255 characters")
     private String description;
 
-    @NotBlank(message = "Contraindications are required")
+    @NotBlank(message = "Contraindications are required", groups = { Create.class})
     @Size(max = 255, message = "Contraindications must be less than 255 characters")
     private String contraindications;
 }
