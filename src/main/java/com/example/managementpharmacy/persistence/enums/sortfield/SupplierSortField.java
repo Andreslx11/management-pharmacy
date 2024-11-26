9ff8050e0b7bc86621feb271c2310ee3433ffb61
Andres Muñoz Taborda <andreslx11@gmail.com>
2024-11-26 17:02:54 -0500

    feat: add SupplierSortField enum to map sort fields to SQL columns
    - Define SupplierSortField enum with mappings between field names and database column names.
    - Include method getSqlColumn to retrieve SQL column name for a given field name, defaulting to 'id_supplier'.

---
+++ src/main/java/com/example/managementpharmacy/persistence/enums/sortfield/SupplierSortField.java
@@ -0,0 +1,34 @@
+package com.example.managementpharmacy.persistence.enums.sortfield;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Arrays;
+
+
+
+
+@Getter
+@AllArgsConstructor
+public enum SupplierSortField {
+
+    ID("id", "id_supplier"),
+    COMPANY_NAME("companyName", "company_name" ),
+    CONTACT("contact", "contact"),
+    PHONE("phone", "phone"),
+    STATE("state", "state");
+
+    private final String fieldName;
+    private final String columnName;
+
+
+    public  static String getSqlColumn(String value){
+        return Arrays.stream(SupplierSortField.values())
+                .filter(sortFiel -> sortFiel.getFieldName().equals(value))
+                .findFirst()
+                .map(SupplierSortField::getColumnName)
+                .orElseGet(ID::getColumnName);
+    }
+
+
+}
