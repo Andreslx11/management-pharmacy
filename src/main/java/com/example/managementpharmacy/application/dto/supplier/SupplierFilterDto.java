ef646e2e9c29b204c8f91059b29d6552632464f7
Andres Muñoz Taborda <andreslx11@gmail.com>
2024-11-26 17:16:24 -0500

    feat: extend SupplierFilterDto with pagination and additional filters
    - Extended SupplierFilterDto to inherit from PageableRequest for pagination support.
    - Added new fields: contact, phone, email, nit, creationDateFrom, and creationDateTo for more detailed filtering options.
    - Applied @DateTimeFormat for creationDateFrom and creationDateTo for proper date formatting.

--- src/main/java/com/example/managementpharmacy/application/dto/supplier/SupplierFilterDto.java
+++ src/main/java/com/example/managementpharmacy/application/dto/supplier/SupplierFilterDto.java
@@ -1,18 +1,29 @@
 package com.example.managementpharmacy.application.dto.supplier;
 
+import com.example.managementpharmacy.shared.page.PageableRequest;
 import com.example.managementpharmacy.shared.state.enums.State;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
-public class SupplierFilterDto {
+@AllArgsConstructor
+@SuperBuilder
+public class SupplierFilterDto extends PageableRequest {
 
         private String companyName;
+        private String contact;
+        private String phone;
+        private String email;
+        private String nit;
         private State state;
 
+        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+        private LocalDate creationDateFrom;
+        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+        private LocalDate  creationDateTo;
 }
