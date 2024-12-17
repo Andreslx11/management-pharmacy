4aab890f3b7bad0571290ac4d4548ab1204139e7
Andres Muñoz Taborda <andreslx11@gmail.com>
2024-12-17 14:10:34 -0500

    feat: add custom exception for authentication errors

---
+++ src/main/java/com/example/managementpharmacy/shared/exception/AuthenticationUserException.java
@@ -0,0 +1,7 @@
+package com.example.managementpharmacy.shared.exception;
+
+public class AuthenticationUserException extends RuntimeException {
+    public AuthenticationUserException(String message) {
+        super(message);
+    }
+}
