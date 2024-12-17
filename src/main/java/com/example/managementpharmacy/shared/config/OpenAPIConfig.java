a7e304861d2713d3c42d40b92966e398489377cd
Andres Muñoz Taborda <andreslx11@gmail.com>
2024-12-17 14:26:49 -0500

    feat: add role-based security to OpenAPI configuration for JWT authentication
    - Implemented role-based security requirements in the OpenAPI configuration.
    - Updated the OpenAPI security scheme to use JWT with bearer token.
    - Allowed access to specific roles (ROLE_USER, ROLE_ADMIN) in the security requirements.
    - Adjusted the authorization checks in API controllers to ensure role-based access control.
    
    OpenAPI configuration now requires authentication using JWT, and roles must be specified in the security requirements to access protected routes.

---
+++ src/main/java/com/example/managementpharmacy/shared/config/OpenAPIConfig.java
@@ -0,0 +1,38 @@
+package com.example.managementpharmacy.shared.config;
+
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+// Stereotype annotation
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customizeOpenApi() {
+        String securitySchemaName = "bearerAuth";
+
+        return new OpenAPI()
+                .addSecurityItem(
+                        new SecurityRequirement()
+                                .addList(securitySchemaName)
+                )
+                .components(
+                        new Components()
+                                .addSecuritySchemes(securitySchemaName, new SecurityScheme()
+                                        .name(securitySchemaName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                )
+                );
+
+    }
+
+
+}
