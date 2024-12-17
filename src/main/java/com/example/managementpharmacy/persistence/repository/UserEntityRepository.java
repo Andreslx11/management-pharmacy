df52885ec881156d076eb83ac7da24987c3a13b4
Andres Muñoz Taborda <andreslx11@gmail.com>
2024-12-17 14:39:34 -0500

    feat: add custom query methods findByUsername and existsByEmployeeId in UserEntityRepository

---
+++ src/main/java/com/example/managementpharmacy/persistence/repository/UserEntityRepository.java
@@ -0,0 +1,15 @@
+package com.example.managementpharmacy.persistence.repository;
+
+
+import com.example.managementpharmacy.persistence.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserEntityRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByUsername(String username);
+
+    boolean existsByEmployeeId(Long employeeId);
+
+}
